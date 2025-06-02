d37859ea7e1ace93bdc0503c2c7e381cea77ea40
Vinayak-cloud-debug <vinayakrodd56@gmail.com>
2025-06-02 22:55:06 +0530

    Update login.js

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -81,61 +81,62 @@ const Login = () => {
       <p className="text-sm text-gray-500 font-medium">Namma Canteen</p>
     </div>
 
-    <form onSubmit={handleSubmit} className="flex flex-col gap-6 w-full">
-      {/* Email */}
-      <div className="flex items-center border border-gray-300 rounded-xl bg-gray-50">
-        <div className="px-4 py-3 text-gray-600">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-envelope">
-            <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
-          </svg>
-        </div>
-        <input
-          type="text"
-          placeholder="Enter your Gmail"
-          className="flex-1 p-3 bg-gray-50 text-black outline-none rounded-r-xl"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
+    <form onSubmit={handleSubmit} className="flex flex-col gap-6 w-full max-w-md mx-auto px-4 sm:px-6 md:px-8">
+  {/* Email */}
+  <div className="flex items-center border border-gray-300 rounded-xl bg-gray-50">
+    <div className="px-4 py-3 text-gray-600">
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-envelope">
+        <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
+      </svg>
+    </div>
+    <input
+      type="text"
+      placeholder="Enter your Gmail"
+      className="flex-1 p-3 bg-gray-50 text-black outline-none rounded-r-xl text-sm sm:text-base"
+      value={username}
+      onChange={(e) => setUsername(e.target.value)}
+    />
+  </div>
 
-      {/* Password */}
-      <div className="flex items-center border border-gray-300 rounded-xl bg-gray-50">
-        <div className="px-4 py-3 text-gray-600">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-key">
-            <path d="M0 8a4 4 0 0 1 7.465-2H14a.5.5 0 0 1 .354.146l1.5 1.5a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0L13 9.207l-.646.647a.5.5 0 0 1-.708 0L11 9.207l-.646.647a.5.5 0 0 1-.708 0L9 9.207l-.646.647A.5.5 0 0 1 8 10h-.535A4 4 0 0 1 0 8m4-3a3 3 0 1 0 2.712 4.285A.5.5 0 0 1 7.163 9h.63l.853-.854a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.793-.793-1-1h-6.63a.5.5 0 0 1-.451-.285A3 3 0 0 0 4 5"/>
-            <path d="M4 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
-          </svg>
-        </div>
-        <input
-          type="password"
-          placeholder="Enter your Password"
-          className="flex-1 p-3 bg-gray-50 text-black outline-none rounded-r-xl"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
+  {/* Password */}
+  <div className="flex items-center border border-gray-300 rounded-xl bg-gray-50">
+    <div className="px-4 py-3 text-gray-600">
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-key">
+        <path d="M0 8a4 4 0 0 1 7.465-2H14a.5.5 0 0 1 .354.146l1.5 1.5a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0L13 9.207l-.646.647a.5.5 0 0 1-.708 0L11 9.207l-.646.647a.5.5 0 0 1-.708 0L9 9.207l-.646.647A.5.5 0 0 1 8 10h-.535A4 4 0 0 1 0 8m4-3a3 3 0 1 0 2.712 4.285A.5.5 0 0 1 7.163 9h.63l.853-.854a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.793-.793-1-1h-6.63a.5.5 0 0 1-.451-.285A3 3 0 0 0 4 5"/>
+        <path d="M4 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
+      </svg>
+    </div>
+    <input
+      type="password"
+      placeholder="Enter your Password"
+      className="flex-1 p-3 bg-gray-50 text-black outline-none rounded-r-xl text-sm sm:text-base"
+      value={password}
+      onChange={(e) => setPassword(e.target.value)}
+    />
+  </div>
 
-      {/* Forgot Link */}
-      <div className="text-right text-sm">
-        <Link to="/forgot-password" className="text-[#e50914] font-semibold hover:underline">
-          Forgot Password?
-        </Link>
-      </div>
+  {/* Forgot Link */}
+  <div className="text-right text-sm">
+    <Link to="/forgot-password" className="text-[#e50914] font-semibold hover:underline">
+      Forgot Password?
+    </Link>
+  </div>
 
-      {/* Login Button */}
-      <button
-        className="w-full bg-[#e50914] hover:bg-red-700 transition-all p-3 rounded-xl font-semibold text-white text-lg"
-      >
-        Login
-      </button>
+  {/* Login Button */}
+  <button
+    className="w-full bg-[#e50914] hover:bg-red-700 transition-all p-3 rounded-xl font-semibold text-white text-base sm:text-lg"
+  >
+    Login
+  </button>
 
-      <p className="text-center text-sm text-gray-600">
-        Don't have an account?{" "}
-        <Link to="/signup" className="text-[#e50914] font-bold hover:underline">
-          Sign Up
-        </Link>
-      </p>
-    </form>
+  <p className="text-center text-sm text-gray-600">
+    Don't have an account?{" "}
+    <Link to="/signup" className="text-[#e50914] font-bold hover:underline">
+      Sign Up
+    </Link>
+  </p>
+</form>
+
 
     <Toaster />
   </div>
